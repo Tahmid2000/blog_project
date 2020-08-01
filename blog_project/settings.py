6a393b014b1f386deb5c81b695d0318b13948479
Tahmid Imran <tahmidimran1@gmail.com>
2020-08-01 17:06:33 -0500

    more changes

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -134,7 +134,7 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
