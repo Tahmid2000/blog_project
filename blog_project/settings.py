61de0a71790316c12dc8d97c6a2acc61aa4600f0
Tahmid Imran <tahmidimran1@gmail.com>
2020-08-02 16:53:26 -0500

    static files

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -132,8 +132,11 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_URL = '/blogs/author/login'
